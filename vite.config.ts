625aa6c084cd97d549ecd002daa83584ddfd98f5
Isaac Ferraro <ieferraro@outlook.com>
2025-01-19 19:31:51 -0800

    Env vars sorted out

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import "jsr:@std/dotenv/load";
 
 export default defineConfig({
   plugins: [solid()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: Deno.env.get("VITE_API_URL"),
         changeOrigin: true,
       },
     },
