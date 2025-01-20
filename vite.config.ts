e6c3c1be8b1a1b93843e9b10bff581450fb15dfa
Isaac Ferraro <ieferraro@outlook.com>
2025-01-19 21:40:55 -0800

    Updated declaration file

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
-import "jsr:@std/dotenv/load";
+import process from "node:process";
 
 export default defineConfig({
   plugins: [solid()],
   server: {
     proxy: {
       "/api": {
-        // deno-lint-ignore no-process-globals
         target: process.env.VITE_API_URL,
         changeOrigin: true,
       },
