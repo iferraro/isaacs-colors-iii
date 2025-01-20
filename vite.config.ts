08662f5b3a4c08260da591071a0ae61278165185
Isaac Ferraro <ieferraro@outlook.com>
2025-01-19 22:25:01 -0800

    Tried site URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
-import process from "node:process";
 
 export default defineConfig({
   plugins: [solid()],
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL,
+        target: "https://isaacscolors.io",
         changeOrigin: true,
       },
     },
